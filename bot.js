6893c5150767ab91e1419dab5fd1a94ada17ade6
vatanstore <45723890+vatanstore@users.noreply.github.com>
2018-12-28 03:59:42 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "N!";
+
+
+client.on('message', message => {
+
+
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '501163717287870467') return;
+
+
+if (message.content.startsWith(prefix + 'playing')) {
+if (message.author.id !== '501163717287870467') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+client.user.setGame(argresult);
+    message.channel.sendMessage(`__**${argresult}**__ : __**تم تغيير الحالة**__ `)
+} 
+
+ 
+if (message.content.startsWith(prefix + 'streem')) {
+if (message.author.id !== '501163717287870467') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+client.user.setGame(argresult, "http://twitch.tv/Mohamed");
+    message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنق`)
+} else
+
+
+
+
+if (message.content.startsWith(prefix + 'watching')) {
+if (message.author.id !== '501163717287870467') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+    client.user.setActivity(argresult, {type : 'watching'});
+ message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
+}
+
+ });
+
+
+
+
+ 
+client.login(process.env.BOT_TOKEN);
